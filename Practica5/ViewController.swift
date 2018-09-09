10d8704067f178b20e26a32ac6b7ce6f87eec703
Alan Olivares <alanolivares@alans-imac.local>
2018-09-09 12:12:29 -0500

    Initial Commit

---
+++ Practica5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica5
+//
+//  Created by Alan Olivares on 09/09/18.
+//  Copyright © 2018 Alan Olivares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
